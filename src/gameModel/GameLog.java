6cbdbbe830e2f881fc59af997b0be3d9d52983a4
Vincent Liu <liuyu-china@hotmail.com>
2018-05-07 01:12:07 +1000

    gameLog

--- src/gameModel/GameLog.java
+++ src/gameModel/GameLog.java
@@ -10,5 +10,34 @@ package gameModel;
  * GameLog defines a structure that will recording the process of running game.
  */
 public class GameLog {
+	private static int turnID; //recording turn's number for each move.
+
+	private PlayerType player;
+	private RoleType role;
+	private int[] coordinate = new int[2];
+
+	public GameLog(PlayerType p, RoleType r, int x, int y){
+		this.player = p;
+		this.role = r;
+		this.coordinate[0] = x;
+		this.coordinate[1] = y;
+	}
+
+	public static int getTurnID() {
+		return turnID;
+	}
+
+	public PlayerType getPlayer() {
+		return player;
+	}
+
+	public RoleType getRole() {
+		return role;
+	}
+
+	public int[] getCoordinate() {
+		return coordinate;
+	}
+
 
 }

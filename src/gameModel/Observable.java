a284490b0af1a020307a05e6351f61e076bafab2
Vincent Liu <liuyu-china@hotmail.com>
2018-05-15 13:22:45 +1000

    Observer Pattern

---
+++ src/gameModel/Observable.java
@@ -0,0 +1,18 @@
+package gameModel;
+
+import gameView.Observer;
+
+/**
+ *
+ * @author Yu Liu
+ * @version 1.0
+ * @since 15/05/2018
+ *
+ *
+ *
+ */
+public interface Observable {
+	public void addObserver(Observer o);
+	public void removeObserver(Observer o);
+	public void notifyObserver();
+}
---
+++ src/gameView/Observer.java
@@ -0,0 +1,13 @@
+package gameView;
+
+/**
+ *
+ * @author Yu Liu
+ * @version 1.0
+ * @since 15/05/2018
+ *
+ *
+ */
+public interface Observer {
+	public void update();
+}

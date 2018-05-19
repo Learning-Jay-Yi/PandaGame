61c7f59dcef8e4cce00a3f20b778b61947a9d20e
Vincent Liu <liuyu-china@hotmail.com>
2018-05-20 02:10:13 +1000

    update

---
+++ src/gameView/Option/HeroPickView.java
@@ -0,0 +1,13 @@
+package gameView.Option;
+
+/**
+ *
+ * @author Yu Liu
+ * @version 1.0
+ * @since 15/05/2018
+ *
+ * HeroPickView include all hero types, and 2 players' containers which are going to save the hero picked
+ */
+public class HeroPickView {
+
+}

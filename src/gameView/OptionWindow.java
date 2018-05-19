a9b024bfd1dd668c278fcc08ce5f336e286fe0e1
lchenyu <liuyu-china@hotmail.com>
2018-05-20 00:48:47 +1000

    update

---
+++ src/gameView/OptionWindow.java
@@ -0,0 +1,33 @@
+package gameView;
+
+import javafx.scene.layout.BorderPane;
+
+/**
+ *
+ * @author Yu Liu
+ * @version 1.0
+ * @since 13/05/2018
+ *
+ * OptionView is a GUI to let player select game type and pick heros
+ *
+ */
+public class OptionWindow {
+
+
+
+	public OptionWindow(){
+
+	}
+
+	public BorderPane displayOption(){
+		BorderPane rootPane = new BorderPane();
+
+		return rootPane;
+	}
+
+
+
+
+
+
+}

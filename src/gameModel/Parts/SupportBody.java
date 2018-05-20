560bbb96325ba6f8da3a7f8194fa8867aea432c4
Learning-Jay-Yi <kboy1122@gmail.com>
2018-05-21 08:06:26 +1000

    Parts

---
+++ src/gameModel/Parts/SupportBody.java
@@ -0,0 +1,56 @@
+package gameModel.Parts;
+
+import gameModel.PlayerType;
+import gameModel.RoleType;
+
+/**
+ * this class is use to make the hero body and spawn it to tile
+ * @author Jay
+ * @version 1.0
+ * @since 2018/5/19
+ */
+public class SupportBody implements PartsBody {
+
+    private int boardWidth, boardHeight;
+    PlayerType playerType;
+    private int spawnX,spawnY;
+    RoleType newHeroType = RoleType.SUPPORT;
+
+    @Override
+    public void PartsBody(int boardWidth, int boardHeight, PlayerType playerType) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        this.playerType= playerType;
+        spawnX = (playerType == PlayerType.BLUE ? 0 : boardWidth-1);
+        spawnY = (playerType == PlayerType.BLUE ? 6 : boardHeight-8);
+//        Spawn();
+    }
+
+    /**
+     * this method use to spawn this hero
+     */
+    @Override
+    public void Spawn() {
+        spawnX = (playerType == PlayerType.BLUE ? 0 : boardWidth-1);
+        spawnY = (playerType == PlayerType.BLUE ? 6 : boardHeight-7);
+
+//        System.out.println("Spawn "+newHeroType+" at "+spawnX+" "+spawnY);
+    }
+    @Override
+    public int getSpawnX() {
+        return spawnX;
+    }
+    @Override
+    public int getSpawnY() {
+        return spawnY;
+    }
+    @Override
+    public PlayerType getPlayerType() {
+        return playerType;
+    }
+    @Override
+    public RoleType getRoleType() {
+        return newHeroType;
+    }
+
+}

305df5dfe96294a156d77494019a8892223875d5
Vincent Liu <liuyu-china@hotmail.com>
2018-05-22 18:55:57 +1000

    tile moutain move

--- src/gameView/ActionSelectWindow.java
+++ src/gameView/ActionSelectWindow.java
@@ -72,7 +72,9 @@ public class ActionSelectWindow {
 		//see TurnCheckerAlarm
 	}
 
-
+	public Stage getWindow(){
+		return action;
+	}
 	private HBox getActionWindow(){
 		HBox buttons = new HBox();
 
@@ -105,6 +107,8 @@ public class ActionSelectWindow {
 			int[] validY = newHero.getPartsAttack().getValidY();
 			int length = validX.length;
 
+
+
 			for (int i = 0; i < length; i++) {
 //				showValidTiles(tileArray,validX[i],validY[i]);
 				int x = validX[i];
@@ -126,6 +130,10 @@ public class ActionSelectWindow {
 			int[] validX = newHero.getPartsMove().getValidX();
 			int[] validY = newHero.getPartsMove().getValidY();
 			int length = validX.length;
+			int oldX = heroView.getLocX();
+			int oldY = heroView.getLocY();
+
+			checkMoveEffect(validX, validY, tileArray, heroView);
 
 			for (int i = 0; i < length; i++) {
 //				showValidTiles(tileArray,validX[i],validY[i]);
@@ -141,4 +149,81 @@ public class ActionSelectWindow {
 		});
 	}
 
+	public Button getMoveBtn(){
+		return moveBtn;
+	}
+
+
+	private void checkMoveEffect(int[] validX, int[] validY, TileView[][] tileArray, HeroView heroView){
+		int oldX = heroView.getLocX();
+		int oldY = heroView.getLocY();
+
+		for(int i = 0; i < validX.length; i++){
+			if(oldX == validX[i]){
+				if(oldY - validY[i] > 0){
+					for(int y = oldY - 1; y > validY[i]; y--){
+						if(tileArray[oldX][y].getEffectValue() == 1){
+							validY[i] = y;
+						}
+					}
+				}else{
+					for(int y = oldY + 1; y < validY[i]; y++){
+						if(tileArray[oldX][y].getEffectValue() == 1){
+							validY[i] = y;
+						}
+					}
+				}
+			}else if(oldY == validY[i]){
+				if(oldX - validX[i] > 0){
+					for(int x = oldX - 1; x > validX[i]; x--){
+						if(tileArray[x][oldY].getEffectValue() == 1){
+							validX[i] = x;
+						}
+					}
+				}else{
+					for(int x = oldX + 1; x < validX[i]; x++){
+						if(tileArray[x][oldY].getEffectValue() == 1){
+							validX[i] = x;
+						}
+					}
+				}
+			}else{
+				if(oldX - validX[i] > 0){
+					if(oldY - validY[i] > 0){
+						for(int x = oldX - 1, y = oldY - 1; x > validX[i]; x--, y--){
+							if(tileArray[x][y].getEffectValue() == 1){
+								validX[i] = x;
+								validY[i] = y;
+							}
+						}
+					}else{
+						for(int x = oldX - 1, y = oldY + 1; x > validX[i]; x--, y++){
+							if(tileArray[x][y].getEffectValue() == 1){
+								validX[i] = x;
+								validY[i] = y;
+							}
+						}
+					}
+				}else{
+					if(oldY - validY[i] > 0){
+						if(oldY - validY[i] > 0){
+							for(int x = oldX + 1, y = oldY - 1; x < validX[i]; x++, y--){
+								if(tileArray[x][y].getEffectValue() == 1){
+									validX[i] = x;
+									validY[i] = y;
+								}
+							}
+						}
+					}else{
+						for(int x = oldX + 1, y = oldY + 1; x < validX[i]; x++, y++){
+							if(tileArray[x][y].getEffectValue() == 1){
+								validX[i] = x;
+								validY[i] = y;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }

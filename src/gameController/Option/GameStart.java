b5441544d538511ce3d158a06b84bf19578702bb
Learning-Jay-Yi <kboy1122@gmail.com>
2018-05-24 14:49:00 +1000

    write file

--- src/gameController/Option/GameStart.java
+++ src/gameController/Option/GameStart.java
@@ -25,31 +25,33 @@ public class GameStart {
 		File file =new File("HeroPoolData.txt");
 	    if(!file.exists())
 	    {
+	    	// if not exists, then create new file and write it into file
 	    	System.out.println("File 'HeroPoolData.txt' does not exit");
 	        System.out.println(file.createNewFile());
 	        file.createNewFile();
 	        dataFileWrite(file);//call the databaseFileRead() method, to initialize data from ArrayList and write data into data.txt
 	    }
-//	    else
-//	    {
-//	    	dataFileRead();//call the databaseFileRead() method, to load data directly from data.txt
-//	    }
+	    else
+	    {
+	    	// if exists, just rewrite it
+			dataFileWrite(file);//call the databaseFileRead() method, to load data directly from data.txt
+	    }
 	}
 
 	private void dataFileWrite(File file) throws IOException{
 
 		FileWriter fw = new FileWriter(file, true);
 		BufferedWriter bw = new BufferedWriter(fw);
-		PrintWriter out = new PrintWriter(bw);
+//		PrintWriter out = new PrintWriter(bw);
 
 		for(HeroPool a : redData){
-			out.write(a.getHeroType()+" "+ a.getAbility()+ " " + "RED");
-			out.flush();
+			bw.write(a.getHeroType()+" "+ a.getAbility()+ " " + "RED");
+			bw.newLine();
 		}
 
 		for(HeroPool b : blueData){
-			out.write(b.getHeroType()+" "+ b.getAbility()+ " " + "BLUE");
-			out.flush();
+			bw.write(b.getHeroType()+" "+ b.getAbility()+ " " + "BLUE");
+			bw.newLine();
 		}
 		bw.close();
 	}
